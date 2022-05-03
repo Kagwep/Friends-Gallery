e69c7a6859d9f2e0660e0777fc205b2ed9a5a8a2
kagwe <kagwepeter07@gmail.com>
2022-05-03 12:01:21 +0300

    'part2'

--- friends.py
+++ friends.py
@@ -3,8 +3,67 @@ from tkinter import filedialog as fd
 from PIL import ImageTk, Image
 import os,shutil
 from tkinter.messagebox import askyesno,showinfo
+from collections import OrderedDict
+import random
+
 
 win = tkinter.Tk()
+
+lbl=tkinter.Label(win,text="   Action panel  ",bg='sea green')
+lbl.grid(column=1,row=0,sticky = "nsew",columnspan=5)
+
+win.title("My Friends Gallery")
+win.geometry("1300x700")
+frame1 = tkinter.Frame(win, padx=5, pady=(5),bg="light sky blue")
+frame1.grid(row=2, column=1,columnspan=6, pady=(10,10),padx=(10,10))
+frame2 = tkinter.Frame(win, padx=5, pady=(5),bg="light sky blue")
+frame2.grid(row=2, column=1,columnspan=6, pady=(10,10),padx=(10,10))
+labellist = []
+labell = []
+def ExitWindow():
+    win.destroy()
+def showImage(event):
+    list1 = os.listdir("images")
+    if len(list1) !=0:
+        image_show = random.choice(list1)
+        image_s = Image.open('images/'+ image_show)
+        photo_s = ImageTk.PhotoImage(image_s)
+        label_s = tkinter.Label(frame2, image=photo_s, text = image_show,compound='top', borderwidth=5 ,relief="ridge")
+        label_s.image = photo_s
+        label_s.grid(row=2,column=5,padx=(10, 10),pady = (10,10))
+        labell.append(label_s)
+        
+    
+    
+def ShowGallery():
+
+    list1 = os.listdir("images")
+    print(list1)
+    row_default = 2
+    column_default = 1
+    for element in list1:
+    
+        print(column_default)
+        key = list1.index(element)
+        image = Image.open('images/'+ element)
+        photo = ImageTk.PhotoImage(image)
+        label = tkinter.Label(frame1, image=photo, text = element,compound='top', borderwidth=5 ,relief="ridge")
+        label.image = photo
+        label.grid(row=row_default,column=column_default,padx=(10, 10),pady = (10,10))
+        labelname= str(label.grid(row=row_default,column=column_default,padx=(10, 10),pady = (10,10)))
+        labellist.append(label)
+        
+    
+        column_default +=1
+        check = column_default % 6
+        if check == 0:
+            row_default += 2
+            column_default = 1
+    print(labellist)
+    
+
+        
+            
 def AddFriend():
     filetypes = (
         ('image files', '*.JPG', '.png'),
@@ -30,7 +89,8 @@ def AddFriend():
                title='Friend added',
                message=filename + ' Added to friends Gallery'
               )
-
+    
+    ShowGallery()
 
 def DeleteFriend():
     filetypes = (
@@ -50,22 +110,36 @@ def DeleteFriend():
             )
         if answer1:
             os.remove(filename)
+            for label in labellist:
+                lab_text = label.cget("text")
+                if lab_text in filename:
+                    label.grid_forget()
+            ShowGallery()
             showinfo(
                title='Deleted',
                message= 'friend ' + filename + 'deleted'
              )
+    
+    
+            
+def ClearGallery():
+    list2 = os.listdir("images")
+    for item in list2:
+        os.remove('images/'+item)
+        for label in labellist:
+            lab_text = label.cget("text")
+            if lab_text in item:
+                label.grid_forget()
+    ShowGallery()
    
-lbl=tkinter.Label(win,text="   Action panel  ")
-lbl.grid(column=1,row=0,sticky = "nsew")
 
-win.title("My Friends Gallery")
-win.geometry("700x350")
-
-b1 = tkinter.Button(win, text = "Show pictures")
-b2 = tkinter.Button(win, text = "Clear Gallery")
-b3 = tkinter.Button(win, text = "Delete a friend",command=DeleteFriend)
-b4 = tkinter.Button(win, text = "Add New Friend", command=AddFriend)
-b5 = tkinter.Button(win, text = "Quit")
+   
+b1 = tkinter.Button(win, text = "Show pictures",bg='light sky blue',command=ShowGallery)
+b1.bind('<Double-1>', showImage) 
+b2 = tkinter.Button(win, text = "Clear Gallery",bg='light sky blue',command=ClearGallery)
+b3 = tkinter.Button(win, text = "Delete a friend",command=DeleteFriend,bg='light sky blue')
+b4 = tkinter.Button(win, text = "Add New Friend", command=AddFriend,bg='light sky blue')
+b5 = tkinter.Button(win, text = "Quit",bg='light sky blue',command=ExitWindow)
                     
 b1.grid(row = 1, column = 1,sticky = "nsew")
 b2.grid(row = 1, column = 2,sticky = "nsew")
@@ -75,25 +149,6 @@ b5.grid(row = 1, column = 5,sticky = "nsew")
 
 
 
-list1 = os.listdir("images")
-
-# Run the above function and store its results in a variable.   
-
-print(list1)
-row_default = 2
-column_default = 1
-for element in list1:
-   print(column_default)
-   key = list1.index(element)
-   image = Image.open('images/'+ element)
-   photo = ImageTk.PhotoImage(image)
-   label = tkinter.Label(win, image=photo, text = element,compound='top', borderwidth=5 )
-   label.image = photo
-   label.grid(row=row_default,column=column_default,padx=(10, 10),pady = (10,10))
-   column_default +=1
-   check = column_default % 6
-   if check == 0:
-      row_default += 2
-      column_default = 1
-      
+ShowGallery()
+win.configure(bg='blue')     
 win.mainloop()
